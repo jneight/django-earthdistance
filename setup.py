cd0ab1806a981afd34ec4239e33d0e2cad0d3f15
Javier Cordero <jcorderomartinez@gmail.com>
2013-11-15 10:34:14 +0100

    Added README.rst to pypi page

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
         'djorm-ext-expressions>=0.5'],
     url='https://github.com/jneight/django-earthdistance',
     description='Add support for PostgreSQL earthdistance extension to Django',
+    long_description=open("README.rst").read(),
     packages=find_packages(),
     include_package_data=True,
     license='Apache 2.0',
