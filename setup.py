d9e620778a8bb49ae8e73ea161fee3e832f1af77
Javier Cordero <javier@corderomartinez.es>
2019-03-28 19:10:40 +0100

    new version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-earthdistance',
-    version='1',
+    version='1.1.0',
     install_requires=[
         'django>=1.8'],
     url='https://github.com/jneight/django-earthdistance',
