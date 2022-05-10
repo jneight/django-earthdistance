bc00f9a7100cdf852a3300148eba310c278ff961
Javier <github@j2i.me>
2022-05-10 17:44:50 +0200

    fix release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-earthdistance',
-    version='1.1.1',
+    version='1.1.3',
     install_requires=[
         'django>=1.8'],
     url='https://github.com/jneight/django-earthdistance',
