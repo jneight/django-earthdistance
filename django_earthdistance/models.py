0fb24cc7f84448091bd4796ec83f1e55ff06a7ce
scottBowles <shbowles@gmail.com>
2024-01-10 15:44:22 -0500

    Fix issue 21 to make it works across foreign key relations

--- django_earthdistance/models.py
+++ django_earthdistance/models.py
@@ -1,48 +1,26 @@
 # coding=utf-8
-
 from django.db import models
+from django.db.models import Func
 
 
-class LlToEarth(models.Func):
-    """
-    Returns the location of a point on the surface of the Earth given its
-    latitude and longitude in degrees.
-    """
-    function = "ll_to_earth"
-    arg_joiner = ", "
-    arity = 1
+class LlToEarth(Func):
+    function = 'll_to_earth'
+
+    def __init__(self, params, output_field=None):
+        if output_field is None:
+            output_field = models.fields.Field()
+        self.params = params
+        super(LlToEarth, self).__init__(*params, output_field=output_field)
 
 
-class EarthDistance(models.Func):
-    """
-    This PostgreSQL function returns the great circle distance between two points on the surface of the
-    earth.
-    """
-    template = 'earth_distance(%(expressions)s)'
+class EarthDistance(Func):
+    function = 'earth_distance'
 
     def __init__(self, expressions, output_field=None):
         if output_field is None:
             output_field = models.fields.Field()
         self.expressions = expressions
-        super(EarthDistance, self).__init__(output_field=output_field)
-
-    def resolve_expression(self, query=None, allow_joins=True, reuse=None, summarize=False, for_save=False):
-        """Prepare SQL from inner funcions (ll_to_earth or any other)"""
-        c = self.copy()
-        c.is_summary = summarize
-        c.for_save = for_save
-        for pos, expression in enumerate(self.expressions):
-            c.expressions[pos] = expression.resolve_expression(query, allow_joins, reuse, summarize)
-        return c
-
-    def as_sql(self, compiler, connection):
-        sql_expressions, sql_params = [], []
-
-        for expression in self.expressions:
-            sql, params = compiler.compile(expression)
-            sql_expressions.append(sql)
-            sql_params.extend(params)
-        return self.template % {'expressions': ','.join(sql_expressions)}, sql_params
+        super(EarthDistance, self).__init__(*expressions, output_field=output_field)
 
 
 class EarthDistanceQuerySet(models.QuerySet):
